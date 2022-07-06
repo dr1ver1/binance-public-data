ae5c2bfb97352a05e1ce68bf7b99a56c655bdf95
dr1ver1 <github@shieff.com>
2022-07-06 13:55:46 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='binance-public-data-dr1ver1',
+    author='dr1ver1',
+    version='0.1.0',
+    description = 'Binance: To help users download our public data easily, we have put all Kline, Trade, and AggTrade data for all pairs, month by month, online.',
+    url='https://github.com/dr1ver1/binance-public-data',
+    packages=find_packages(include=['python', 'python.*', 'Futures_Order_Book_Download', 'Futures_Order_Book_Download.*', 'data', 'data.*']),
+    install_requires=[
+        'pandas'
+    ],
+    # setup_requires=['pytest-runner', 'flake8'],
+    # tests_require=['pytest'],
+    # entry_points={
+    #     'console_scripts': ['my-command=exampleproject.example:main']
+    # },
+    # package_data={'exampleproject': ['data/*.txt']}
+)
